98a545df9170d18b6c8f86f34274f3dfe49ee846
vasana1 <vasana1@yahoo.com>
2017-03-29 21:39:44 -1000

    finished 8

--- js/app.js
+++ js/app.js
@@ -43,29 +43,49 @@ function valueMenu(){
 }
 
 //5. Gin.
-/*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
+/*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
 
 function redFace(){
-
+  var changeText=document.getElementById("drink");
+  changeText.style.color = "red";
+  changeText.style.fontSize = "30px";
 }
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+  var addPrice=document.getElementById("price");
+  addPrice.innerHTML="$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+Benjamin.addEventListener("click", saySomething);
+
+function saySomething(){
+  var newQuote=document.getElementById("displayQuote");
+  newQuote.innerHTML = myQuote;
+}
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+random.addEventListener("click", randomQuote);
 
+function randomQuote(){
+  var randomQuo = quotes[Math.floor(Math.random()*quotes.length)];
+
+ displayQuotes.innerHTML = randomQuo;
+
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
+
